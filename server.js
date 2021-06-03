88bdaf2cfd334650a2148e71ebfacb920abf971d
Abdul Moiz Siraj <abdulmoiz68@gmail.com>
2021-06-03 21:38:11 +0500

    final server

--- server.js
+++ server.js
@@ -25,32 +25,86 @@ const http = require('http').Server(app);
 app.use(express.json({ extended: false, limit: '50mb' }));
 
 app.post('/upload', upload.single('photo'), (req, res, next) => {
-  // res.json(req.file);
-  // console.log(timeNow);
-  // //send name to fastapi
-  // var request = require('request');
-  // var clientServerOptions = {
-  //   url: 'http://192.168.1.106:8000/filename?fname=' + timeNow,
-  //   method: 'POST',
-  //   headers: {
-  //     'Content-Type': 'application/json',
-  //   },
-  // };
-  // request(clientServerOptions, function (error, response) {
-  //   console.log(error);
-  //   return;
-  // });
-
   const axios = require('axios');
 
   var text = '';
+  var textNew = '';
+  var textFinal = '';
   const sendPostRequest = async () => {
-    let url = 'http://192.168.1.106:8000/filename?fname=' + timeNow;
+    let url = 'http://192.168.0.108:5000/filename?fname=' + timeNow;
     try {
       const resp = await axios.post(url);
 
       text = JSON.stringify(resp.data);
+      let textArray = text.split(/\r?\n/);
+      textNew = textArray.join(' ');
 
+      for (let index = 0; index < textNew.length - 1; index++) {
+        if (
+          textNew.charAt(index) === '\\' &&
+          textNew.charAt(index + 1) === 'n'
+        ) {
+          textFinal = textFinal + ' ';
+          index++;
+        } else {
+          textFinal = textFinal + textNew.charAt(index);
+        }
+      }
+
+      console.log(textFinal);
+    } catch (err) {
+      // Handle Error Here
+      console.error(err);
+    }
+  };
+
+  sendPostRequest();
+
+  setTimeout(() => {
+    // console.log(text);
+    const data = JSON.stringify({
+      txt: textFinal,
+    });
+    res.send(data);
+  }, 3000);
+});
+
+app.post('/uploadText', upload.single('photo'), (req, res, next) => {
+  const axios = require('axios');
+
+  var text = '';
+  const sendPostRequest = async () => {
+    let url = 'http://192.168.1.103:5000/filenameText?fname=' + timeNow;
+    try {
+      const resp = await axios.post(url);
+
+      text = JSON.stringify(resp.data);
+    } catch (err) {
+      // Handle Error Here
+      console.error(err);
+    }
+  };
+
+  sendPostRequest();
+
+  setTimeout(() => {
+    const data = JSON.stringify({
+      txt: text,
+    });
+    res.send(data);
+  }, 3000);
+});
+
+app.get('/hello', (req, res, next) => {
+  const axios = require('axios');
+
+  var text = '';
+  const sendGetRequest = async () => {
+    let url = 'http://10.0.0.4:5000/hello' + timeNow;
+    try {
+      const resp = await axios.get(url);
+
+      text = JSON.stringify(resp.data);
       console.log(text);
     } catch (err) {
       // Handle Error Here
@@ -58,8 +112,7 @@ app.post('/upload', upload.single('photo'), (req, res, next) => {
     }
   };
 
-  sendPostRequest();
-
+  sendGetRequest();
   // const data = JSON.stringify({
   //   txt: text,
   // });
@@ -74,40 +127,6 @@ app.post('/upload', upload.single('photo'), (req, res, next) => {
   }, 3000);
 });
 
-app.post('/uploadText', upload.single('photo'), (req, res, next) => {
-  //res.json(req.file);
-  //send name to fastapi
-  const axios = require('axios');
-
-  var text = '';
-  const sendPostRequest = async () => {
-    let url = 'http://192.168.1.106:8000/filenameText?fname=' + timeNow;
-    try {
-      const resp = await axios.post(url);
-
-      text = JSON.stringify(resp.data);
-    } catch (err) {
-      // Handle Error Here
-      console.error(err);
-    }
-  };
-
-  sendPostRequest();
-
-  // const data = JSON.stringify({
-  //   txt: text,
-  // });
-  // res.send(data);
-
-  setTimeout(() => {
-    // console.log(text);
-    const data = JSON.stringify({
-      txt: text,
-    });
-    res.send(data);
-  }, 3000);
-});
-
-const PORT = 5000;
+const PORT = 8000;
 app.get('/', (req, res) => res.send('API running'));
 app.listen(PORT, () => console.log(`Server running on Port : ${PORT}`));
